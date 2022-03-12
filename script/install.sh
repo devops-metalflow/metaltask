49b5b11c7d8f7d17089f9360dc5d0761be191d0b
Jia <49358172+craftslab@users.noreply.github.com>
2022-03-12 13:30:57 +0800

    Add support to build target for stable-x86_64-unknown-linux-musl

--- script/install.sh
+++ script/install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Install for Windows target
+# Update for stable
+rustup update --no-self-update stable
+
+# Install for coverage
+cargo install cargo-tarpaulin
+
+# Install for lint
+cargo install cargo-hack --debug
+rustup component add clippy
+rustup component add rustfmt
+
+# Install for upx
+sudo apt update -y
+sudo apt install -y upx
+
+# Install for Windows
 # rustc --print target-list
-rustup target add x86_64-pc-windows-gnu
-rustup toolchain install stable-x86_64-pc-windows-gnu
 sudo apt update -y
 sudo apt install -y mingw-w64
+rustup target add x86_64-pc-windows-gnu
+rustup toolchain install stable-x86_64-pc-windows-gnu
 
-# Install for Linux target
+# Install for Linux
 # rustc --print target-list
-rustup target add x86_64-unknown-linux-gnu
-rustup toolchain install stable-x86_64-unknown-linux-gnu
+rustup target add x86_64-unknown-linux-musl
+rustup toolchain install stable-x86_64-unknown-linux-musl
