b5f964a9a558bc1d9168382b0f4b80e883b6a37a
Jia Jia <angersax@sina.com>
2023-02-03 10:40:39 +0800

    refactor(build): add support to build in offline mode

--- script/build.sh
+++ script/build.sh
@@ -9,6 +9,7 @@ if [ "$1" = "all" ]; then
   build=$build cargo build --release --all-features --all-targets --target=x86_64-pc-windows-gnu
   build=$build cargo build --release --all-features --all-targets --target=x86_64-unknown-linux-musl
 elif [ "$1" = "offline" ]; then
+  build=$build cargo build --release --all-features --all-targets --target=x86_64-pc-windows-gnu --offline
   build=$build cargo build --release --all-features --all-targets --target=x86_64-unknown-linux-musl --offline
 elif [ "$1" = "check" ]; then
   build=$build cargo check --release --all-features --all-targets
